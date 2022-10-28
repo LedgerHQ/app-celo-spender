ff42375ad11268ddeee48f7065a8df1b04097ef0
Sarah GLINER <sarah.gliner@ledger.fr>
2022-10-28 10:05:51 +0200

    tests: add relevant files

---
+++ tests/python/apps/celo.py
@@ -0,0 +1,93 @@
+from typing import List, Generator
+from enum import IntEnum
+from contextlib import contextmanager
+
+from ragger.backend.interface import BackendInterface, RAPDU
+
+from .celo_cmd_builder import *
+import rlp
+
+
+class INS(IntEnum):
+    INS_GET_PUBLIC_KEY = 0x02
+    INS_SIGN = 0x04
+    INS_GET_APP_CONFIGURATION = 0x06
+    INS_SIGN_PERSONAL_MESSAGE = 0x08
+    INS_PROVIDE_ERC20_TOKEN_INFORMATION = 0x0A
+    INS_GET_APP_TYPE = 0x0C
+
+CLA = 0xE0
+
+MAX_CHUNK_SIZE = 255
+
+
+class StatusCode(IntEnum):
+    STATUS_OK = 0x9000
+
+
+class Param(IntEnum):
+    P1_DirectlyFetchAddress     = 0x00 # Return address directly from the wallet
+    P1_ShowFetchAddress         = 0x01 # Return address from the wallet after showing it 
+    P1_InitTransactionData      = 0x00 # First transaction data block for signing
+    P1_ContTransactionData      = 0x80 # Subsequent transaction data block for signing
+    P2_DiscardAddressChainCode  = 0x00 # Do not return the chain code along with the address
+    P1_UNUSED                   = 0x00 # Default value
+    P2_UNUSED                   = 0x00 # Default value
+
+
+class CeloClient:
+    backend: BackendInterface
+
+    def __init__(self, backend):
+        self._client = backend
+
+
+    def get_version(self) -> bytes:
+        version: RAPDU = self._client.exchange(CLA, INS.INS_GET_APP_CONFIGURATION,
+                                                  Param.P1_UNUSED, Param.P2_UNUSED)
+        return version
+
+
+    @contextmanager
+    def send_in_chunk_async(self, instruction: int, payload: bytes) -> Generator[None, None, None]:
+        p1 = Param.P1_InitTransactionData
+        while (len(payload) > 0):
+            chunk = MAX_CHUNK_SIZE
+            if chunk > len(payload):
+                chunk = len(payload)
+
+            with self._client.exchange_async(CLA, instruction, p1, Param.P2_UNUSED, payload[:chunk]):
+                yield
+
+            payload = payload[chunk:]
+            p1 = Param.P1_ContTransactionData
+                
+
+    @contextmanager
+    def derive_address_async(self, derivation_path: bytes, show: bool, chaincode: bool) -> Generator[None, None, None]:
+        p1 = Param.P1_ShowFetchAddress if show else Param.P1_DirectlyFetchAddress
+        p2 = Param.P2_UNUSED if chaincode else Param.P2_DiscardAddressChainCode
+
+        with self._client.exchange_async(CLA, INS.INS_GET_PUBLIC_KEY, p1, p2, derivation_path):
+           yield
+
+
+    @contextmanager
+    def sign_data_async(self, derivation_path: bytes, data: str) -> Generator[None, None, None]:
+        payload: bytes = derivation_path
+        payload += len(data).to_bytes(4, byteorder='big')
+        payload += str.encode(data)
+
+        with self.send_in_chunk_async(INS.INS_SIGN_PERSONAL_MESSAGE, payload):
+            yield
+
+
+    @contextmanager
+    def sign_transaction_async(self, derivation_path : bytes, arg_list : List[str]) -> Generator[None, None, None]:
+        encoded = rlp.encode(arg_list)
+
+        payload: bytes = derivation_path
+        payload += encoded
+
+        with self.send_in_chunk_async(INS.INS_SIGN, payload):
+            yield
---
+++ tests/python/test_celo.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from .apps.celo import CeloClient, StatusCode
+from .apps.celo_utils import CELO_PACKED_DERIVATION_PATH
+from .utils import get_async_response, get_nano_review_instructions, get_stax_review_instructions, get_stax_review_instructions_with_warning
+from ragger.navigator import NavInsID, NavIns
+
+import pytest
+
+TESTS_ROOT_DIR = Path(__file__).parent
+
+
+@pytest.mark.parametrize("show", [False, True])
+@pytest.mark.parametrize("chaincode", [False, True])
+def test_celo_derive_address(test_name, backend, firmware, show, chaincode, navigator): 
+    celo = CeloClient(backend)
+
+    if firmware.device == "nanos":
+        instructions = get_nano_review_instructions(4)
+    elif firmware.device.startswith("nano"):
+        instructions = get_nano_review_instructions(2)
+    else:
+        instructions = [
+            NavIns(NavInsID.USE_CASE_CHOICE_CONFIRM),
+            NavIns(NavInsID.TOUCH, (200, 335)),
+            NavIns(NavInsID.USE_CASE_ADDRESS_CONFIRMATION_EXIT_QR),
+            NavIns(NavInsID.USE_CASE_ADDRESS_CONFIRMATION_CONFIRM),
+            NavIns(NavInsID.USE_CASE_STATUS_DISMISS)
+        ]
+
+    with celo.derive_address_async(CELO_PACKED_DERIVATION_PATH, show, chaincode):
+        if show:
+            navigator.navigate_and_compare(TESTS_ROOT_DIR,
+                                           test_name,
+                                           instructions)
+
+    response: bytes = get_async_response(backend)
+
+    assert (response.status == StatusCode.STATUS_OK)
+
+
+def test_celo_get_version(backend, firmware):
+    celo = CeloClient(backend)
+    response = celo.get_version()
+
+    assert (response.status == StatusCode.STATUS_OK)
+
+
+def test_sign_data(test_name, backend, firmware, navigator):
+    celo = CeloClient(backend)
+    if firmware.device.startswith("nano"):
+        instructions = get_nano_review_instructions(2)
+    else:
+        instructions = get_stax_review_instructions(1)
+
+    with celo.sign_data_async(CELO_PACKED_DERIVATION_PATH, "1234567890"):
+        navigator.navigate_and_compare(TESTS_ROOT_DIR, test_name, instructions)
+
+    response: bytes = get_async_response(backend)
+
+    assert (response.status == StatusCode.STATUS_OK)
+    assert (len(response.data) == 65)
