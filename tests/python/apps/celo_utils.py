7755d03d70e5aea5ba9b27d7db83b4e18f545f1e
keiff3r <keiffer@blooo.io>
2024-03-12 18:19:53 +0100

    fix: Update ETH_PACKED_DERIVATION_PATH in celo_utils.py
    changed the format from 5 derivation to 3 derivations

--- tests/python/apps/celo_utils.py
+++ tests/python/apps/celo_utils.py
@@ -1,4 +1,4 @@
 from ragger.bip import pack_derivation_path
 
 CELO_PACKED_DERIVATION_PATH = pack_derivation_path("m/44'/52752'/12345'")
-ETH_PACKED_DERIVATION_PATH = pack_derivation_path("m/44'/60'/0'/0/0")
+ETH_PACKED_DERIVATION_PATH = pack_derivation_path("m/44'/60'/0'")
