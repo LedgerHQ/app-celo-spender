1eb83d272b90c145615fe8e1172e552791c9ab11
keiff3r <keiffer@blooo.io>
2024-03-20 18:43:52 +0100

    refactor: rename a test and generate screenshots accordingly

--- tests/python/test_sign_eip1559.py
+++ tests/python/test_sign_eip1559.py
@@ -13,7 +13,7 @@ TX_TYPE_EIP1559 = 2
 TX_TYPE_CIP64 = 123
 
 
-def sign_transaction_1559(test_name, backend, navigator, instructions, rawTx):
+def sign_transaction_with_rawTx(test_name, backend, navigator, instructions, rawTx):
     celo = CeloClient(backend)
     # transaction_params.append("") # ADD DONE PARAMETER
     with celo.sign_transaction_with_rawTx_async(
@@ -28,12 +28,13 @@ def sign_transaction_1559(test_name, backend, navigator, instructions, rawTx):
 
 
 
-def test_sign_transaction_123(test_name, backend, firmware, navigator):
+def test_sign_transaction_eip1559(test_name, backend, firmware, navigator):
     if firmware.device == "nanos":
         instructions = get_nano_review_instructions(9)
     elif firmware.device.startswith("nano"):
         instructions = get_nano_review_instructions(5)
     else:
         instructions = get_stax_review_instructions_with_warning(1)
+
     rawTx = "02f8d482a4ec820808839b34b4850fbc63d144830204e094004626a008b1acdc4c74ab51644093b155e59a2380b864ba0876520000000000000000000000000000000000000000000000009458660c5b865f23000000000000000000000000e3b72489968f11c15282514f33df24634440393f000000000000000000000000e3b72489968f11c15282514f33df24634440393fc001a0b0799073a2aa771c5e32b88933ff19982dc30f9e4523fde47137ae504793b880a07014a6e3c32a3b34d4118beb298f2200e858599b5e97766dfaa6fea192cde993"
-    sign_transaction_1559(test_name, backend, navigator, instructions, rawTx)
+    sign_transaction_with_rawTx(test_name, backend, navigator, instructions, rawTx)
