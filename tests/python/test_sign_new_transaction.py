0666526d602e8a9627b2c7232afdd56235aeb0dc
keiff3r <keiffer@blooo.io>
2024-03-22 13:34:25 +0100

    test: rename file and add test structure for cip64

--- tests/python/test_sign_new_transaction.py
+++ tests/python/test_sign_new_transaction.py
@@ -38,3 +38,14 @@ def test_sign_transaction_eip1559(test_name, backend, firmware, navigator):
 
     rawTx = "02f8d482a4ec820808839b34b4850fbc63d144830204e094004626a008b1acdc4c74ab51644093b155e59a2380b864ba0876520000000000000000000000000000000000000000000000009458660c5b865f23000000000000000000000000e3b72489968f11c15282514f33df24634440393f000000000000000000000000e3b72489968f11c15282514f33df24634440393fc001a0b0799073a2aa771c5e32b88933ff19982dc30f9e4523fde47137ae504793b880a07014a6e3c32a3b34d4118beb298f2200e858599b5e97766dfaa6fea192cde993"
     sign_transaction_with_rawTx(test_name, backend, navigator, instructions, rawTx)
+
+def test_sign_transaction_cip64(test_name, backend, firmware, navigator):
+    if firmware.device == "nanos":
+        instructions = get_nano_review_instructions(9)
+    elif firmware.device.startswith("nano"):
+        instructions = get_nano_review_instructions(5)
+    else:
+        instructions = get_stax_review_instructions_with_warning(1)
+
+    rawTx = "7b"
+    sign_transaction_with_rawTx(test_name, backend, navigator, instructions, rawTx)
