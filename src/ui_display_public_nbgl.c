a5fef5fc35ff4ce6d40b6468e202ed2fd903b1bb
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2024-08-29 10:51:01 +0200

    feat(ui):  Update NBGL UI based on new API

--- src/ui_display_public_nbgl.c
+++ src/ui_display_public_nbgl.c
@@ -4,13 +4,13 @@
 #include "bolos_target.h"
 
 static void address_cancel(void) {
-    nbgl_useCaseStatus("Address rejected", false, ui_idle);
+    nbgl_useCaseReviewStatus(STATUS_TYPE_ADDRESS_REJECTED, ui_idle);
     io_seproxyhal_touch_address_cancel();
 }
 
 static void address_confirmation(bool confirm) {
     if (confirm) {
-        nbgl_useCaseStatus("ADDRESS\nAPPROVED", true, ui_idle);
+        nbgl_useCaseReviewStatus(STATUS_TYPE_ADDRESS_VERIFIED, ui_idle);
         io_seproxyhal_touch_address_ok();
     }
     else {
@@ -18,17 +18,13 @@ static void address_confirmation(bool confirm) {
     }
 }
 
-static void address_display(void) {
-    nbgl_useCaseAddressConfirmation(strings.common.fullAddress, address_confirmation);
-}
-
 void ui_display_public_flow(void) {
-    nbgl_useCaseReviewStart(&C_celo_64px,
-                            "Verify Celo\naddress",
-                            "",
-                            "Cancel",
-                            address_display,
-                            address_cancel);
+    nbgl_useCaseAddressReview(tmpCtx.publicKeyContext.address,
+                              NULL,
+                              &C_celo_64px,
+                              "Verify Celo\naddress",
+                              NULL,
+                              address_confirmation);
 }
 
 #endif // HAVE_NBGL
