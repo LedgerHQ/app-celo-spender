133310bb1c887e321d43253dc53f6dfc9a6b53ca
dbaranov-hodies <dmitrii.baranov@hoodies.team>
2026-05-28 14:44:10 +0400

    fmt

--- src/celo.c
+++ src/celo.c
@@ -381,13 +381,13 @@ void finalizeParsing(bool direct) {
         memcpy(tmpContent.txContent.value.value, dataContext.unlockContext.data + 4, 32);
         tmpContent.txContent.value.length = 32;
     } else if (provisionType == PROVISION_RELOCK) {
-      memcpy(tmpContent.txContent.value.value, dataContext.relockContext.data + 4 + 32, 32);
-      tmpContent.txContent.value.length = 32;
+        memcpy(tmpContent.txContent.value.value, dataContext.relockContext.data + 4 + 32, 32);
+        tmpContent.txContent.value.length = 32;
     } else if (provisionType == PROVISION_WITHDRAW) {
-      memcpy(tmpContent.txContent.value.value, dataContext.withdrawContext.data + 4, 32);
-      tmpContent.txContent.value.length = 32;
+        memcpy(tmpContent.txContent.value.value, dataContext.withdrawContext.data + 4, 32);
+        tmpContent.txContent.value.length = 32;
     } else if (provisionType == PROVISION_CREATE_ACCOUNT) {
-      // no parameters to extract; selector only
+        // no parameters to extract; selector only
     } else {
         if (dataPresent && !N_storage.dataAllowed && provisionType != PROVISION_LOCK) {
             reset_app_context();
